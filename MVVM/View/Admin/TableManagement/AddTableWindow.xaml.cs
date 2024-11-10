b393bded05120c8f05d262cf9e1b8769af032a10
huenguyentran <huenguyentran3110@gmail.com>
2024-11-10 09:44:48 +0700

    TableUC_ControlBarUC

---
+++ MVVM/View/Admin/TableManagement/AddTableWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLiCoffeeShop.MVVM.View.Admin.TableManagament
+{
+    /// <summary>
+    /// Interaction logic for AddTableWindow.xaml
+    /// </summary>
+    public partial class AddTableWindow : Window
+    {
+        public AddTableWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OutlinedComboBox_ColorChanged(object sender, RoutedPropertyChangedEventArgs<Color> e)
+        {
+
+        }
+    }
+}
