a37cbfe28a6578521a8c63f1935fb02ffecdea63
huenguyentran <huenguyentran3110@gmail.com>
2024-11-11 09:37:42 +0700

    ErrorManagement

---
+++ MVVM/View/Admin/ErrorManagement/AddErrorWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLiCoffeeShop.MVVM.View.Admin.ErrorManagement
+{
+    /// <summary>
+    /// Interaction logic for AddErrorWindow.xaml
+    /// </summary>
+    public partial class AddErrorWindow : Window
+    {
+        public AddErrorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
