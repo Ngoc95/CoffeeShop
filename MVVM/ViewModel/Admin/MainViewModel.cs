cba3fdb59a61742f835b56ad4694c834e92a8ac8
Dương Khánh Ngọc <duongkhanhngoc2005@gmail.com>
2024-11-14 18:28:39 +0700

    Add ControlbarX
    add minimize and close button, update RelayCommands

--- MVVM/ViewModel/Admin/MainViewModel.cs
+++ MVVM/ViewModel/Admin/MainViewModel.cs
@@ -4,17 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace QuanLiCoffeeShop.MVVM.ViewModel.Admin
 {
     class MainViewModel : ObservableObject
     {
-        public RelayCommand CustomerViewCommand { get; set; }
-        public RelayCommand EmployeeViewCommand { get; set; }
-        public RelayCommand ErrorViewCommand { get; set; }
-        public RelayCommand MenuViewCommand { get; set; }
-        public RelayCommand TableViewCommand { get; set; }
-        public CustomerViewModel CustomerVM {  get; set; }
+        public ICommand CustomerViewCommand { get; set; }
+        public ICommand EmployeeViewCommand { get; set; }
+        public ICommand ErrorViewCommand { get; set; }
+        public ICommand MenuViewCommand { get; set; }
+        public ICommand TableViewCommand { get; set; }
+        public CustomerViewModel CustomerVM { get; set; }
         public EmployeeViewModel EmployeeVM { get; set; }
         public ErrorViewModel ErrorVM { get; set; }
         public MenuViewModel MenuVM { get; set; }
@@ -25,9 +27,9 @@ namespace QuanLiCoffeeShop.MVVM.ViewModel.Admin
         public object CurrentView
         {
             get { return _currentView; }
-            set 
-            { 
-                _currentView = value; 
+            set
+            {
+                _currentView = value;
                 OnPropertyChanged();
             }
 
@@ -35,37 +37,19 @@ namespace QuanLiCoffeeShop.MVVM.ViewModel.Admin
 
         public MainViewModel()
         {
-           CustomerVM = new CustomerViewModel();
-           EmployeeVM = new EmployeeViewModel();
-           ErrorVM = new ErrorViewModel();
-           MenuVM = new MenuViewModel();
-           TableVM = new TableViewModel();
+            CustomerVM = new CustomerViewModel();
+            EmployeeVM = new EmployeeViewModel();
+            ErrorVM = new ErrorViewModel();
+            MenuVM = new MenuViewModel();
+            TableVM = new TableViewModel();
 
-           CurrentView = EmployeeVM;
+            CurrentView = EmployeeVM;
 
-            CustomerViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = CustomerVM;
-            });
-
-            EmployeeViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = EmployeeVM;
-            });
-
-            ErrorViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = ErrorVM;
-            });
-
-            MenuViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = MenuVM;
-            });
-            TableViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = TableVM;
-            });
+            CustomerViewCommand = new RelayCommand<ContentControl>((p) => { return true; }, (p) => { CurrentView = CustomerVM; });
+            EmployeeViewCommand = new RelayCommand<ContentControl>((p) => { return true; }, (p) => { CurrentView = EmployeeVM; });
+            ErrorViewCommand = new RelayCommand<ContentControl>((p) => { return true; }, (p) => { CurrentView = ErrorVM; });
+            MenuViewCommand = new RelayCommand<ContentControl>((p) => { return true; }, (p) => { CurrentView = MenuVM; });
+            TableViewCommand = new RelayCommand<ContentControl>((p) => { return true; }, (p) => { CurrentView = TableVM; });
         }
     }
 }
