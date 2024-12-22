01431a6561d685b0d8f6dc58fa0a667591776fca
ngoczoro <daongoc.work@gmail.com>
2024-12-22 22:14:29 +0700

    ThongKe
    add ThongKe

---
+++ Converters/CusNameConverter.cs
@@ -0,0 +1,34 @@
+﻿using QuanLiCoffeeShop.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace QuanLiCoffeeShop.Converters
+{
+    public class CusNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type TargetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || ((CUSTOMER)value).CUS_NAME == null
+                )
+            {
+                string st = "Khách vãng lai";
+                return st;
+            }
+            else
+            {
+
+                return ((CUSTOMER)value).CUS_NAME;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
