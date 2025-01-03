a7d9cea8e0f003ef84bbdb6220298e154e0cec61
huenguyentran <huenguyentran3110@gmail.com>
2025-01-03 10:33:03 +0700

    final

--- MVVM/ViewModel/Admin/MenuViewModel.cs
+++ MVVM/ViewModel/Admin/MenuViewModel.cs
@@ -363,6 +363,7 @@ namespace QuanLiCoffeeShop.MVVM.ViewModel.Admin
                     UpdateGenPrdNameList();
                     p.Text = "";
                     ischeckedAddTggBtn = false;
+                    NextGenPrdID++;
                 }
             });
 
